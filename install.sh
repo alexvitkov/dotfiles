15630075b4d93ab9171e3c308765677685bc015d
Alex Vitkov <alexvitkov98@gmail.com>
2021-03-08 10:06:53 +0200

    asdf

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+mkdir -p ~/.config/nvim/colors
+
 ln -sf "$DOTFILES/bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES/bash_profile" "$HOME/.bash_profile"
-
-mkdir -p ~/.config/nvim/colors
-ln -sf "$DOTFILES/vimcolors" "$HOME/.config/nvim/colors/good.vim"
-ln -sf "$DOTFILES/vimrc"     "$HOME/.config/nvim/init.vim"
-ln -sf "$DOTFILES/sxhkdrc"   "$HOME/.config/sxhkd/sxhkdrc"
+ln -sf "$DOTFILES/vimcolors"    "$HOME/.config/nvim/colors/good.vim"
+ln -sf "$DOTFILES/vimrc"        "$HOME/.config/nvim/init.vim"
+ln -sf "$DOTFILES/sxhkdrc"      "$HOME/.config/sxhkd/sxhkdrc"
+ln -sf "$DOTFILES/picom.conf"   "$HOME/.config/picom.conf"
+ln -sf "$DOTFILES/xinitrc"      "$HOME/.xinitrc"           
