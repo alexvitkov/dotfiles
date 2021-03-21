c3a0496dbb738a19c7678130a7d41029045d3d3b
Alex Vitkov <alexvitkov98@gmail.com>
2021-03-21 09:40:13 +0200

    asdf

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config/sxhkd
 
 mkdir -p ~/.config/wal/templates
 mkdir -p ~/.config/rofi
+mkdir -p ~/.config/dunst
 cp "$DOTFILES"/wal/* ~/.config/wal/templates
 
 
@@ -17,6 +18,7 @@ ln -sf "$DOTFILES/gdbinit"      "$HOME/.gdbinit"
 ln -sf "$DOTFILES/tmux.conf"    "$HOME/.tmux.conf"         
 ln -sf "$DOTFILES/roficonfig"   "$HOME/.config/rofi/config"
 ln -sf ~/.cache/wal/my.rofi     "$HOME/.config/rofi/theme.rasi"
+ln -sf ~/.cache/wal/my.dunstrc  "$HOME/.config/dunst/dunstrc"
 
 ln -sf "$(which rofi)"          "$DOTFILES/bin/dmenu"
 
